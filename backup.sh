1691e4da1ea676bc1492da3b1438db0267a6bdd7
Praveen Kumar <pravinblaze@hotmail.com>
2024-04-24 23:54:42 +0200

    Fix backup.sh

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@
 cp ~/.zshrc ./
 mkdir -p ./.oh-my-zsh/custom/themes/
 cp ~/.oh-my-zsh/custom/themes/my.zsh-theme ./.oh-my-zsh/custom/themes/
-cp -r ~/.config/nvim/ ./
-rm nvim/lazy-lock.json
+cp -r ~/.config/nvim/ ./.config/
+rm ./config/nvim/lazy-lock.json
 mkdir -p ./.config/tmux/
 cp ~/.config/tmux/tmux.conf ./.config/tmux/
 mkdir -p ./.config/alacritty/
